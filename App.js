95ad2848dcff7d6a1f2eb8c6b8a093ddb65a01bc
Shau Yin Luk <ivan.luk@icloud.com>
2020-09-30 15:56:17 -0500

    react native state

--- App.js
+++ App.js
@@ -1,21 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Button, StyleSheet, View, Text } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class ButtonBasics extends Component {
+  state = {
+    textDisp: "ok"
+  }
+
+  componentDidMount(){
+    this.setState({
+      textDisp: "Button Pressed"
+    })
+    console.log(this.state.textDisp)
+  } 
+  _onPressButton() {
+    // this.setState({
+    //   textDisp: "Button Pressed"
+    // })
+    alert('You tapped the button!')
+  }
+  
+  render() {
+    console.log(this.state.textDisp)
+    return (
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+            color="#841584"
+          />
+        </View>
+        <View style={styles.alternativeLayoutButtonContainer}>
+          <Button
+            onPress={() => {this.setState({textDisp: "Great!"})}}
+            title="This looks great!"
+          />
+          <Button
+            onPress={() => {this.setState({textDisp: "OK Pressed"})}}
+            title="OK!"
+            color="#841584"
+          />
+        </View>
+        <View>
+          <Text>{this.state.textDisp}</Text>
+        </View>
+      </View>
+    );
+  }
 }
+
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
+   flex: 1,
+   justifyContent: 'center',
   },
+  buttonContainer: {
+    margin: 20
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
 });

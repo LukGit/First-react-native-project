1161fd4504fbed4b094acec1990a5d5b5933a609
Shau Yin Luk <ivan.luk@icloud.com>
2020-10-01 11:13:32 -0500

    scrolling and styling

--- App.js
+++ App.js
@@ -1,5 +1,26 @@
 import React, { Component } from 'react';
-import { Button, StyleSheet, View, Text } from 'react-native';
+import { Button, StyleSheet, View, Text, ScrollView, Image } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   justifyContent: 'center',
+  },
+  buttonContainer: {
+    margin: 20
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  textStyle: {
+    color: "red",
+    fontFamily: "Helvetica-Bold",
+    fontSize: 20,
+    textAlign: "center"
+  }
+});
 
 export default class ButtonBasics extends Component {
   state = {
@@ -23,7 +44,7 @@ export default class ButtonBasics extends Component {
     console.log(this.state.textDisp)
     return (
       <View style={styles.container}>
-        <View style={styles.buttonContainer}>
+        {/* <View style={styles.buttonContainer}>
           <Button
             onPress={this._onPressButton}
             title="Press Me"
@@ -35,38 +56,59 @@ export default class ButtonBasics extends Component {
             title="Press Me"
             color="#841584"
           />
-        </View>
+        </View> */}
         <View style={styles.alternativeLayoutButtonContainer}>
           <Button
             onPress={() => {this.setState({textDisp: "Great!"})}}
-            title="This looks great!"
+            title={this.state.textDisp}
           />
           <Button
             onPress={() => {this.setState({textDisp: "OK Pressed"})}}
             title="OK!"
-            color="#841584"
+            color="black" 
           />
         </View>
-        <View>
-          <Text>{this.state.textDisp}</Text>
+        <View style={styles.buttonContainer}>
+          <Text
+          style={styles.textStyle}
+          >{this.state.textDisp}</Text>
         </View>
+        <ScrollView>
+          <Text style={styles.textStyle}>Scroll me plz</Text>
+          <Image source={{uri: "https://reactnative.dev/img/tiny_logo.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://reactnative.dev/img/tiny_logo.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://reactnative.dev/img/tiny_logo.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://reactnative.dev/img/tiny_logo.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://reactnative.dev/img/tiny_logo.png", width: 64, height: 64}} />
+          <Text style={styles.textStyle}>If you like</Text>
+          <Image source={{uri: "https://reactnative.dev/img/tiny_logo.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://reactnative.dev/img/tiny_logo.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://reactnative.dev/img/tiny_logo.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://reactnative.dev/img/tiny_logo.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://reactnative.dev/img/tiny_logo.png", width: 64, height: 64}} />
+          <Text style={styles.textStyle}>Scrolling down</Text>
+          <Image source={{uri: "https://reactnative.dev/img/tiny_logo.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://reactnative.dev/img/tiny_logo.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://reactnative.dev/img/tiny_logo.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://reactnative.dev/img/tiny_logo.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://reactnative.dev/img/tiny_logo.png", width: 64, height: 64}} />
+          <Text style={styles.textStyle}>What's the best</Text>
+          <Image source={{uri: "https://reactnative.dev/img/tiny_logo.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://reactnative.dev/img/tiny_logo.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://reactnative.dev/img/tiny_logo.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://reactnative.dev/img/tiny_logo.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://reactnative.dev/img/tiny_logo.png", width: 64, height: 64}} />
+          <Text style={styles.textStyle}>Framework around?</Text>
+          <Image source={{uri: "https://reactnative.dev/img/tiny_logo.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://reactnative.dev/img/tiny_logo.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://reactnative.dev/img/tiny_logo.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://reactnative.dev/img/tiny_logo.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://reactnative.dev/img/tiny_logo.png", width: 64, height: 64}} />
+          <Text style={styles.textStyle}>React Native</Text>
+        </ScrollView>
       </View>
     );
   }
 }
 
 
-const styles = StyleSheet.create({
-  container: {
-   flex: 1,
-   justifyContent: 'center',
-  },
-  buttonContainer: {
-    margin: 20
-  },
-  alternativeLayoutButtonContainer: {
-    margin: 20,
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  }
-});
